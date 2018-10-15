53958b47ec82e8e09d9d5b4b2bfd7733caa8f748
raoyy <r799081367@qq.com>
2018-10-15 16:37:22 +0800

    修正路由传参错误

--- voy_wx/voy-wx/src/components/volunteer/actLi.vue
+++ voy_wx/voy-wx/src/components/volunteer/actLi.vue
@@ -1,6 +1,6 @@
 <template>
 	<li>
-    	<a href="javascript:;" @click="toActmess" >
+    	<a href="javascript:;" @click="toActmess(actLi.id)" >
         	<div class="voyLr" :style="{backgroundImage:cover}"></div>
         	<div class="voyLL">
            		<div class="voyLtitile">{{actLi.title}}</div>
